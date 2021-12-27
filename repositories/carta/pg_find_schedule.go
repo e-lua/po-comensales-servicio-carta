b3846c2c68f306c47f2a88db7214b9d75e4a2f2f
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-27 15:34:32 -0500

    Cambiamos el struct

--- repositories/carta/pg_find_schedule.go
+++ repositories/carta/pg_find_schedule.go
@@ -10,7 +10,7 @@ func Pg_Find_ScheduleRange(date string, idbusiness int) ([]models.Pg_ScheduleLis
 
 	db := models.Conectar_Pg_DB()
 
-	q := "SELECT ls.idschedule,ls.starttime::time,NOW()::time at time zone CONCAT('UTC',(ls.timezone::integer*-1)::varchar(3)) ,CONCAT(c.date::date,' ',ls.starttime)::timestamp,CONCAT(c.date::date,' ',ls.endtime)::timestamp,ls.maxorders,CONCAT(ls.starttime,' - ',ls.endtime) FROM listschedulerange ls LEFT JOIN carta c ON ls.idcarta=c.idcarta WHERE c.date::date=$1::date AND ls.idbusiness=$2 AND ls.maxorders>0 AND concat(ls.starttime,ls.timezone)::time with time zone>NOW()::time at time zone CONCAT('UTC',(ls.timezone::integer*-1)::varchar(3)) ORDER BY REPLACE(ls.starttime,':','')::int  ASC"
+	q := "SELECT ls.idschedule,c.date::date,ls.starttime::time,ls.endtime::time,ls.timezone,ls.maxorders,CONCAT(ls.starttime,' - ',ls.endtime) FROM listschedulerange ls LEFT JOIN carta c ON ls.idcarta=c.idcarta WHERE c.date::date=$1::date AND ls.idbusiness=$2 AND ls.maxorders>0 AND concat(ls.starttime,ls.timezone)::time with time zone>NOW()::time at time zone CONCAT('UTC',(ls.timezone::integer*-1)::varchar(3)) ORDER BY REPLACE(ls.starttime,':','')::int  ASC"
 	rows, error_shown := db.Query(context.Background(), q, date, idbusiness)
 
 	//Instanciamos una variable del modelo Pg_TypeFoodXBusiness
