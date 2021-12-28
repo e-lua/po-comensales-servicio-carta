9cf3938533f23e56bf4a5355bcf51158ee61483e
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-28 14:56:04 -0500

    Se puso Stock en la resta de stock enves de quantity

--- repositories/element/pg_update_stock.go
+++ repositories/element/pg_update_stock.go
@@ -12,7 +12,7 @@ func Pg_Update_Stock(elements_stock models.Pg_ToElement_Mqtt) error {
 	db := models.Conectar_Pg_DB()
 
 	for i := 0; i < elements_stock.IDElement[len(elements_stock.IDElement)-1]; i++ {
-		query := `UPDATE Element SET quantity=quantity-$1 WHERE idelement=$2 AND idcarta=$3`
+		query := `UPDATE Element SET stock=stock-$1 WHERE idelement=$2 AND idcarta=$3`
 		if _, err := db.Exec(context.Background(), query, elements_stock.Quantity[i], elements_stock.IDElement[i], elements_stock.IDCarta[i]); err != nil {
 			return err
 		}
