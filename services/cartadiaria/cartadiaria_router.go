f7f797d440085e93fb32d54f604d3455d0e9a59e
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-07-15 00:44:23 -0500

    v1.56

--- services/cartadiaria/cartadiaria_router.go
+++ services/cartadiaria/cartadiaria_router.go
@@ -269,7 +269,7 @@ func (cr *cartaDiariaRouter_pg) GetBusinessInformation_V2(c echo.Context) error 
 	//idbusiness_int, _ := strconv.Atoi(idbusiness)
 
 	//Enviamos los datos al servicio de anfitriones para obtener los datos completos
-	respuesta, _ := http.Get("http://a-informacion.restoner-api.fun:5800/v1/business/comensal/bnss/" + idbusiness)
+	respuesta, _ := http.Get("http://a-informacion.restoner-api.fun:80/v1/business/comensal/bnss/" + idbusiness)
 	var get_respuesta ResponseBusiness_V2
 	error_decode_respuesta := json.NewDecoder(respuesta.Body).Decode(&get_respuesta)
 	if error_decode_respuesta != nil {
