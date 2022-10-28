8dcbf1417d1541294858b9dd0dc58d36239ea4e0
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-10-28 15:45:26 -0500

    v1.114

--- repositories/cartadiaria/pg_find_elements.go
+++ repositories/cartadiaria/pg_find_elements.go
@@ -46,7 +46,7 @@ func Pg_Find_Elements_SearchByText(date string, idbusiness int, text string, lim
 
 	db := models.Conectar_Pg_DB()
 
-	q := "SELECT e.idelement,e.idbusiness,e.idcategory,e.namecategory,e.urlphotcategory,e.name,e.price,e.description,e.urlphoto,e.typemoney,e.stock,e.typefood,e.insumos,e.availableorders,e.costo FROM element e LEFT JOIN carta c ON e.idcarta=c.idcarta WHERE c.date=$1 AND e.idbusiness=$2 AND lower(e.name) ~ $3 ORDER BY e.name ASC LIMIT $4 OFFSET $5"
+	q := "SELECT e.idelement,e.idbusiness,e.idcategory,e.namecategory,e.urlphotcategory,e.name,e.price,e.description,e.urlphoto,e.typemoney,e.stock,e.typefood,e.insumos,e.availableorders,e.costo,e.additionals FROM element e LEFT JOIN carta c ON e.idcarta=c.idcarta WHERE c.date=$1 AND e.idbusiness=$2 AND lower(e.name) ~ $3 ORDER BY e.name ASC LIMIT $4 OFFSET $5"
 	rows, error_shown := db.Query(ctx, q, date, idbusiness, text, limit, offset)
 
 	//Instanciamos una variable del modelo Pg_TypeFoodXBusiness
@@ -60,7 +60,7 @@ func Pg_Find_Elements_SearchByText(date string, idbusiness int, text string, lim
 	//Scaneamos l resultado y lo asignamos a la variable instanciada
 	for rows.Next() {
 		var oElement models.Pg_Element_ToCreate
-		rows.Scan(&oElement.IDElement, &oElement.IDBusiness, &oElement.IDCategory, &oElement.NameCategory, &oElement.UrlPhotoCategory, &oElement.Name, &oElement.Price, &oElement.Description, &oElement.UrlPhoto, &oElement.TypeMoney, &oElement.Stock, &oElement.TypeFood, &oElement.Insumos, &oElement.AvailableOrders, &oElement.Costo)
+		rows.Scan(&oElement.IDElement, &oElement.IDBusiness, &oElement.IDCategory, &oElement.NameCategory, &oElement.UrlPhotoCategory, &oElement.Name, &oElement.Price, &oElement.Description, &oElement.UrlPhoto, &oElement.TypeMoney, &oElement.Stock, &oElement.TypeFood, &oElement.Insumos, &oElement.AvailableOrders, &oElement.Costo, &oElement.Additionals)
 		oListElementsWithStock = append(oListElementsWithStock, oElement)
 	}
 
@@ -139,7 +139,7 @@ func Pg_Web_Find_Elements_SearchByText(date string, idbusiness int, name string,
 
 	db := models.Conectar_Pg_DB()
 
-	q := "SELECT e.idelement,e.idbusiness,e.idcategory,e.namecategory,e.urlphotcategory,e.name,e.price,e.description,e.urlphoto,e.typemoney,e.stock,e.typefood,e.insumos,e.availableorders,e.costo FROM element e LEFT JOIN carta c ON e.idcarta=c.idcarta WHERE c.date=$1 AND e.idbusiness=$2 AND (lower(e.name) ~ $3 OR lower(e.description) ~ $3) ORDER BY e.name ASC LIMIT $4"
+	q := "SELECT e.idelement,e.idbusiness,e.idcategory,e.namecategory,e.urlphotcategory,e.name,e.price,e.description,e.urlphoto,e.typemoney,e.stock,e.typefood,e.insumos,e.availableorders,e.costo,e.additionals FROM element e LEFT JOIN carta c ON e.idcarta=c.idcarta WHERE c.date=$1 AND e.idbusiness=$2 AND (lower(e.name) ~ $3 OR lower(e.description) ~ $3) ORDER BY e.name ASC LIMIT $4"
 	rows, error_shown := db.Query(ctx, q, date, idbusiness, name, limit)
 
 	//Instanciamos una variable del modelo Pg_TypeFoodXBusiness
@@ -153,7 +153,7 @@ func Pg_Web_Find_Elements_SearchByText(date string, idbusiness int, name string,
 	//Scaneamos l resultado y lo asignamos a la variable instanciada
 	for rows.Next() {
 		var oElement models.Pg_Element_ToCreate
-		rows.Scan(&oElement.IDElement, &oElement.IDBusiness, &oElement.IDCategory, &oElement.NameCategory, &oElement.UrlPhotoCategory, &oElement.Name, &oElement.Price, &oElement.Description, &oElement.UrlPhoto, &oElement.TypeMoney, &oElement.Stock, &oElement.TypeFood, &oElement.Insumos, &oElement.AvailableOrders, &oElement.Costo)
+		rows.Scan(&oElement.IDElement, &oElement.IDBusiness, &oElement.IDCategory, &oElement.NameCategory, &oElement.UrlPhotoCategory, &oElement.Name, &oElement.Price, &oElement.Description, &oElement.UrlPhoto, &oElement.TypeMoney, &oElement.Stock, &oElement.TypeFood, &oElement.Insumos, &oElement.AvailableOrders, &oElement.Costo, &oElement.Additionals)
 		oListElementsWithStock = append(oListElementsWithStock, oElement)
 	}
 
